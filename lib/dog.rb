e180a98e2ffbfcb5f739edbb79ca4c8e4e536e17
Jenise Ruehle <jela07@gmail.com>
2020-08-10 19:11:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -60,6 +60,12 @@ class Dog
     SQL
     
     dog = DB[:conn].execute(sql, name, breed).first
+    if dog 
+      new_dog = self.new_from_db(dog)
+    else 
+      new_dog = self.create(name: name, breed: breed)
+    end 
+      new_dog
   end 
   
   def self.find_by_name(name)
