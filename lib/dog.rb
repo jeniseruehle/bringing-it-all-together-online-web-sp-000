1f8db7a7cecfc5aeb8167f3cd49c7de49df26243
Jenise Ruehle <jela07@gmail.com>
2020-08-10 19:03:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -40,8 +40,8 @@ class Dog
   
   def self.new_from_db(row)
    attributes_hash = {
-    id: row[0]
-    name: row[1]
+    id: row[0],
+    name: row[1],
     breed: row[2]
    }
     Dog.new(attributes_hash)
